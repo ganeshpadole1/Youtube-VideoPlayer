da93d72e3ef74071791a642772398b31936b85fb
ganesh padole <ganeshpadole55@gmail.com>
2020-07-05 19:55:30 +0530

    Added Segue Identifier

--- Youtube-VideoPlayer/LoginViewController/LoginViewController.swift
+++ Youtube-VideoPlayer/LoginViewController/LoginViewController.swift
@@ -7,24 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailtextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginAction(_ sender: UIButton) {
+        
+        if let email = emailtextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+              
+                if let error = error {
+                    self?.showAlert(title: "Alert", message: error.localizedDescription)
+                } else {
+                    self?.performSegue(withIdentifier: "LoginToYoutubeVideo", sender: self)
+                }
+              
+            }
+        }
     }
-    */
-
 }
