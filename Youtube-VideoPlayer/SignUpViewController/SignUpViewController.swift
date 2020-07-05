e11ea4f20efa46dc63e0564cc4f9e169300424bf
ganesh padole <ganeshpadole55@gmail.com>
2020-07-05 19:27:44 +0530

    Successfully registered user in firebase.

--- Youtube-VideoPlayer/SignUpViewController/SignUpViewController.swift
+++ Youtube-VideoPlayer/SignUpViewController/SignUpViewController.swift
@@ -7,24 +7,36 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var registerUserButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerUser(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                
+                if let error = error {
+                    self.showAlert(title: "Alert", message: error.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToYoutube", sender: self)
+                }
+            }
+        }
     }
-    */
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
 }
